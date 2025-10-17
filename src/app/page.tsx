46ccb7be8ac3b90725ac1119f944c5cc65643352
Florian <florian.chouquet@epitech.eu>
2025-10-17 16:50:29 +0200

    style(page.tsx): Adding responsiveness

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,15 +9,18 @@ import { FSocialNetworks } from "../components/cardSocialNetworks";
 
 export default function FHome() {
   return (
-    <main className="bg-gray-800">
-      <FHeader />
-      <FNavigationMenu />
-      <FSocialNetworks />
-      <FAbout />
-      <FExperiences />
-      <FProjects />
-      <FEducation />
-      <FFooter />
+    <main className="bg-gray-800 flex flex-col md:flex-row min-h-screen">
+      <div className="w-full md:w-1/3 p-6 bg-gray-900 text-gray-100">
+        <FHeader />
+        <FNavigationMenu />
+        <FSocialNetworks />
+      </div>
+      <div className="w-full md:w-2/3 p-6 space-y-8 bg-gray-900 text-gray-100">
+        <FAbout />
+        <FExperiences />
+        <FProjects />
+        <FEducation />
+        <FFooter />
+      </div>
     </main>);
 }
-
