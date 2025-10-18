1b1e3c1618f83ac930b10411479f4b6f044555d9
Florian <florian.chouquet@epitech.eu>
2025-10-18 18:41:53 +0200

    fix(cardFooter.tsx): Unifixing footer

--- src/components/cardFooter.tsx
+++ src/components/cardFooter.tsx
@@ -1,6 +1,6 @@
 export function FFooter() {
     return (
-        <footer id="footer" className="fixed bottom-0 left-0 w-full py-4 bg-gray-900 hover:bg-gray-800/50 transition-colors duration-200 p-6 rounded-lg">
+        <footer id="footer" className="bottom-0 left-0 w-full py-4 bg-gray-900 hover:bg-gray-800/50 transition-colors duration-200 p-6 rounded-lg">
             <p className="text-gray-100">Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec qu</p>
         </footer>
     );
