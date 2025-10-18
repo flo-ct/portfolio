e18fa77b21479b30fc60861489bf6ef8e2d4b275
Florian <florian.chouquet@epitech.eu>
2025-10-18 11:01:47 +0200

    feat(page.tsx): Make right column static

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,12 +10,12 @@ import { FSocialNetworks } from "../components/cardSocialNetworks";
 export default function FHome() {
   return (
     <main className="bg-gray-800 flex flex-col md:flex-row min-h-screen">
-      <div className="w-full md:w-1/3 p-6 bg-gray-900 text-gray-100">
+      <div className="w-full md:w-1/3 p-6 bg-gray-900 text-gray-100 md:fixed md:h-screen md:overflow-y-auto">
         <FHeader />
         <FNavigationMenu />
         <FSocialNetworks />
       </div>
-      <div className="w-full md:w-2/3 p-6 space-y-8 bg-gray-900 text-gray-100">
+      <div className="w-full md:ml-[33.3333%] w-2/3 p-6 space-y-8 bg-gray-900 text-gray-100">
         <FAbout />
         <FExperiences />
         <FProjects />
